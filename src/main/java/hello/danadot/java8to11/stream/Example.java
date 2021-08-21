c55950a0b93ee828daf4f8ade001854fc3adf8d2
danaRyu <dana40426@gmail.com>
2021-08-21 22:51:42 +0900

    [Stream] StreamAPI 예제1

--- src/main/java/hello/danadot/java8to11/stream/Example.java
+++ src/main/java/hello/danadot/java8to11/stream/Example.java
@@ -1,8 +1,12 @@
 package hello.danadot.java8to11.stream;
 
+import javax.swing.*;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Example {
 
@@ -15,6 +19,43 @@ public class Example {
         springClasses.add(new OnlineClass(4, "Spring core", false));
         springClasses.add(new OnlineClass(5, "Rest Api Development", false));
 
+        System.out.println("1. Spring으로 시작하는 수업 ");
+
+/*
+        List<String> classList =
+            springClasses.stream().map((onlineClass) -> {
+                if(onlineClass.getTitle().startsWith("Spring")) {
+                    return onlineClass.getTitle();
+                }
+                return null;
+            }).collect(Collectors.toList());
+        classList.forEach(System.out::println);
+*/
+
+        springClasses.stream()
+                .filter(onlineClass -> onlineClass.getTitle().startsWith("Spring"))
+                .forEach(onlineClass -> System.out.print(onlineClass.getId() + " "));
+
+        System.out.println("\n2. close 되지 않은 수업");
+        springClasses.stream()
+                .filter(onlineClass -> !onlineClass.isClosed())
+                .forEach(onlineClass -> System.out.print(onlineClass.getId() + " "));
+
+        System.out.println();
+        //method reference로 단축
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(onlineClass -> System.out.print(onlineClass.getId() + " "));
+
+        System.out.println("\n3. 수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+                .map(onlineClass -> onlineClass.getTitle())
+                .forEach(System.out::println);
+
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
+        
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java, Test", true));
         javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
@@ -23,15 +64,6 @@ public class Example {
         List<List<OnlineClass>> danadotEvents = new ArrayList<>();
         danadotEvents.add(springClasses);
         danadotEvents.add(javaClasses);
-
-        System.out.println("Spring으로 시작하는 수업 ");
-        // TODO
-
-        System.out.println("close 되지 않은 수업");
-        // TODO
-
-        System.out.println("수업 이름만 모아서 스트림 만들기");
-        // TODO
 
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
         // TODO
