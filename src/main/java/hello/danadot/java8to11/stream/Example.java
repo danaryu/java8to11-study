351365ace191c27a289637e742e8f60860e41d2e
danaRyu <dana40426@gmail.com>
2021-08-25 22:27:21 +0900

    StreamAPI 예제 실습

--- src/main/java/hello/danadot/java8to11/stream/Example.java
+++ src/main/java/hello/danadot/java8to11/stream/Example.java
@@ -1,8 +1,10 @@
 package hello.danadot.java8to11.stream;
 
 import javax.swing.*;
+import java.math.BigDecimal;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -55,7 +57,7 @@ public class Example {
         springClasses.stream()
                 .map(OnlineClass::getTitle)
                 .forEach(System.out::println);
-        
+
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java, Test", true));
         javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
@@ -65,14 +67,40 @@ public class Example {
         danadotEvents.add(springClasses);
         danadotEvents.add(javaClasses);
 
-        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
-        // TODO
+        System.out.println("4. 두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        /*
+        danadotEvents.stream()
+            .forEach(onlineClassList -> onlineClassList
+                    .forEach(onlineClass -> System.out.println(onlineClass.getTitle())));
 
-        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
-        // TODO
+         */
 
-        System.out.println("자바 수업중에 Test가 들어있는 수업이 있는지 확인");
-        // TODO
+        danadotEvents.stream().flatMap(Collection::stream)
+                     .forEach(onlineClass -> System.out.println(onlineClass.getId()));
+
+        System.out.println("5. 10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(BigDecimal.ZERO, num -> num.add(BigDecimal.ONE))
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("6. 자바 수업중에 Test가 들어있는 수업이 있는지 확인");
+/*        javaClasses.stream()
+                .filter(javaClass -> javaClass.getTitle().contains("Test"))
+                .forEach(javaClass -> System.out.println(javaClass.getTitle()));
+*/
+
+        boolean anyMatch = javaClasses.stream().anyMatch(onlineClass -> onlineClass.getTitle().contains("Test"));
+        System.out.println("anyMatch = " + anyMatch);
+
+        System.out.println("7. 스프링 수업중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+
+        List<String> spring = springClasses.stream()
+                .filter(onlineClass -> onlineClass.getTitle().contains("Spring"))
+                .map(OnlineClass::getTitle)
+                .collect(Collectors.toList());
+        spring.forEach(System.out::println);
+
 
     }
 
