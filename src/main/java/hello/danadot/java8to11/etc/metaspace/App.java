8836706d547f81184050bc2bc42e0ce63238b295
danaRyu <dana40426@gmail.com>
2021-08-28 22:53:26 +0900

    etc: parallel array & metaspace

---
+++ src/main/java/hello/danadot/java8to11/etc/metaspace/App.java
@@ -0,0 +1,19 @@
+package hello.danadot.java8to11.etc.metaspace;
+
+public class App {
+
+    // JVM의 여러 메모리 영역 중에 PermGen 메모리 영역이 없어지고, MetaSpace 영역이 생겼다.
+
+    /**
+     *  1. permGen
+        * Heap영역에 속함.
+        * 기본 값으로 제한된 크기를 가지고 있음.
+     */
+
+    /**
+     * 2. Metaspace
+        *
+        *
+     */
+
+}
